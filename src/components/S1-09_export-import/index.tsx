b1bab56f4a2f310600bf088471dbfd816391f62f
Jhonnatan Douglas <jhonnatanaraujodev@gmail.com>
2024-06-21 16:39:29 -0300

    import export

---
+++ src/components/S1-09_export-import/index.tsx
@@ -0,0 +1,14 @@
+import ExportComponent2 from "./index2";
+
+const ExportComponent = () => {
+  return (
+    <>
+      <div>
+        <p>testando</p>
+        <ExportComponent2 />
+      </div>
+    </>
+  );
+};
+
+export default ExportComponent;
